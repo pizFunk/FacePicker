78d82a2ae1bc7fdc1e7eb4bbf68a20586a9f4caf
matthew <pizFunk@admins-mac.local>
2018-09-05 07:50:28 -0500

    Initial Commit

---
+++ FacePickerTests/FacePickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  FacePickerTests.swift
+//  FacePickerTests
+//
+//  Created by matthew on 9/5/18.
+//  Copyright © 2018 matthew. All rights reserved.
+//
+
+import XCTest
+@testable import FacePicker
+
+class FacePickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
